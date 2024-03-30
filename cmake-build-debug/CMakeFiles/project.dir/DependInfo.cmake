e72e9cc5f9f5154d4ee005e78aab6caaa164d19e
ni <2412248477@qq.com>
2024-03-30 22:18:00 +0800

    改了cmake

---
+++ cmake-build-debug/CMakeFiles/project.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Defender/Desktop/project/work/database/database.cpp" "CMakeFiles/project.dir/database/database.cpp.obj" "gcc" "CMakeFiles/project.dir/database/database.cpp.obj.d"
+  "C:/Users/Defender/Desktop/project/work/main.cpp" "CMakeFiles/project.dir/main.cpp.obj" "gcc" "CMakeFiles/project.dir/main.cpp.obj.d"
+  "C:/Users/Defender/Desktop/project/work/parser/parser.cpp" "CMakeFiles/project.dir/parser/parser.cpp.obj" "gcc" "CMakeFiles/project.dir/parser/parser.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/database/database.cpp.obj"
+  "CMakeFiles/project.dir/database/database.cpp.obj.d"
+  "CMakeFiles/project.dir/main.cpp.obj"
+  "CMakeFiles/project.dir/main.cpp.obj.d"
+  "CMakeFiles/project.dir/parser/parser.cpp.obj"
+  "CMakeFiles/project.dir/parser/parser.cpp.obj.d"
+  "libproject.dll.a"
+  "project.exe"
+  "project.exe.manifest"
+  "project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
