b7f65a7ef51794989139e16629ded7923c949afb
ni <2412248477@qq.com>
2024-04-27 16:36:24 +0800

    再改exporter

--- exporter/exporter.cpp
+++ exporter/exporter.cpp
@@ -27,24 +27,24 @@ void exporter::exportPlacementData(const placement& placementData, std::vector<n
       for (int j_pmos = 0; j_pmos < exporterNet[i]->num_pmos; j_pmos++) {
         std::string pmos_key = "pmos" + std::to_string(j_pmos);
         j["placement"][pmos_key] = {
-            {"x", exporterNet[i]->pmos[j_pmos]->m_x},
-            {"y", exporterNet[i]->pmos[j_pmos]->m_x}, // Corrected from m_x to m_y
-            {"source", exporterNet[i]->pmos[j_pmos]->m_source},
-            {"gate", exporterNet[i]->pmos[j_pmos]->m_gate},
-            {"drain", exporterNet[i]->pmos[j_pmos]->m_drain},
-            {"width", exporterNet[i]->pmos[j_pmos]->m_wide}
+            {"x", (*exporterNet[i]->pmos[j_pmos]).m_x},
+            {"y", (*exporterNet[i]->pmos[j_pmos]).m_x}, // 要改
+            {"source", (*exporterNet[i]->pmos[j_pmos]).m_source},
+            {"gate", (*exporterNet[i]->pmos[j_pmos]).m_gate},
+            {"drain", (*exporterNet[i]->pmos[j_pmos]).m_drain},
+            {"width", (*exporterNet[i]->pmos[j_pmos]).m_wide}
         };
       }
 
       for (int j_nmos = 0; j_nmos < exporterNet[i]->num_nmos; j_nmos++) {
         std::string nmos_key = "nmos" + std::to_string(j_nmos);
         j["placement"][nmos_key] = {
-            {"x", exporterNet[i]->nmos[j_nmos]->m_x},
-            {"y", exporterNet[i]->nmos[j_nmos]->m_x}, // Corrected from m_x to m_y
-            {"source", exporterNet[i]->nmos[j_nmos]->m_source},
-            {"gate", exporterNet[i]->nmos[j_nmos]->m_gate},
-            {"drain", exporterNet[i]->nmos[j_nmos]->m_drain},
-            {"width", exporterNet[i]->nmos[j_nmos]->m_wide}
+            {"x", (*exporterNet[i]->nmos[j_nmos]).m_x},
+            {"y", (*exporterNet[i]->nmos[j_nmos]).m_x}, // 要改
+            {"source", (*exporterNet[i]->nmos[j_nmos]).m_source},
+            {"gate", (*exporterNet[i]->nmos[j_nmos]).m_gate},
+            {"drain", (*exporterNet[i]->nmos[j_nmos]).m_drain},
+            {"width", (*exporterNet[i]->nmos[j_nmos]).m_wide}
         };
       }
     }
