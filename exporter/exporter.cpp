5ea5f8b8639a91808693524bb8912f219b4f5965
gooooog <2412248477@qq.com>
2024-05-11 12:03:56 +0800

    完成输出了

--- exporter/exporter.cpp
+++ exporter/exporter.cpp
@@ -13,49 +13,38 @@
 exporter::exporter(std::vector<net*> network) :exportNet(network){};
 
 void exporter::exportPlacementData(std::vector<net*> exporterNet) {
-  std::ofstream outFile("C:/Users/Defender/Desktop/project/work/placementData.json");
-  if(!outFile.is_open())
-  {
-    std::cout << "fail to open the file" << std::endl;
-  }
-  else
-  {
-    std::cout << " open the file successfully" << std::endl;
-    nlohmann::json j;
-    j["placement"] = nlohmann::json::object();
-
-    for (int i = 0; i < exporterNet.size(); i++) {
-      for (int j_pmos = 0; j_pmos < exporterNet[i]->num_pmos; j_pmos++) {
-        std::string pmos_key = "pmos" + std::to_string(j_pmos);
-        j["placement"][pmos_key] = {
-            {"x", (*exporterNet[i]->pmos[j_pmos]).m_x},
-            {"y", (*exporterNet[i]->pmos[j_pmos]).m_x}, // 要改
-            {"source", (*exporterNet[i]->pmos[j_pmos]).m_source},
-            {"gate", (*exporterNet[i]->pmos[j_pmos]).m_gate},
-            {"drain", (*exporterNet[i]->pmos[j_pmos]).m_drain},
-            {"width", (*exporterNet[i]->pmos[j_pmos]).m_wide}
-        };
-      }
-      outFile << j;
-      outFile << std::endl;
-
-      nlohmann::json k;
-      k["placement"] = nlohmann::json::object();
-      for (int j_nmos = 0; j_nmos < exporterNet[i]->num_nmos; j_nmos++) {
-        std::string nmos_key = "nmos" + std::to_string(j_nmos);
-        k["placement"][nmos_key] = {
-            {"x", (*exporterNet[i]->nmos[j_nmos]).m_x},
-            {"y", (*exporterNet[i]->nmos[j_nmos]).m_x}, // 要改
-            {"source", (*exporterNet[i]->nmos[j_nmos]).m_source},
-            {"gate", (*exporterNet[i]->nmos[j_nmos]).m_gate},
-            {"drain", (*exporterNet[i]->nmos[j_nmos]).m_drain},
-            {"width", (*exporterNet[i]->nmos[j_nmos]).m_wide}
-        };
-      }
-      outFile << k;
-      outFile << std::endl;
+    std::ofstream outFile("C:/Users/Defender/Desktop/project/work/placementData.json");
+    if(!outFile.is_open())
+    {
+        std::cout << "fail to open the file" << std::endl;
     }
+    else
+    {
+        std::cout << " open the file successfully and successful output" << std::endl;
+        nlohmann::ordered_json j; // 使用ordered_json代替json
 
-  }
-  outFile.close();
+        for (int i = 0; i < exporterNet.size(); i++) {
+            for (int j_pmos = 0; j_pmos < exporterNet[i]->num_pmos; j_pmos++) {
+                j["placement"][exporterNet[i]->pmos[j_pmos]->m_name]["x"] = std::to_string(exporterNet[i]->pmos[j_pmos]->m_x);
+                j["placement"][exporterNet[i]->pmos[j_pmos]->m_name]["y"] = "1";
+                j["placement"][exporterNet[i]->pmos[j_pmos]->m_name]["source"] = exporterNet[i]->pmos[j_pmos]->m_source;
+                j["placement"][exporterNet[i]->pmos[j_pmos]->m_name]["gate"] = exporterNet[i]->pmos[j_pmos]->m_gate;
+                j["placement"][exporterNet[i]->pmos[j_pmos]->m_name]["drain"] = exporterNet[i]->pmos[j_pmos]->m_drain;
+                j["placement"][exporterNet[i]->pmos[j_pmos]->m_name]["width"] = std::to_string(exporterNet[i]->pmos[j_pmos]->m_wide);
+            }
+            for (int j_nmos = 0; j_nmos < exporterNet[i]->num_nmos; j_nmos++) {
+                j["placement"][exporterNet[i]->nmos[j_nmos]->m_name]["x"] = std::to_string(exporterNet[i]->nmos[j_nmos]->m_x);
+                j["placement"][exporterNet[i]->nmos[j_nmos]->m_name]["y"] = "0";
+                j["placement"][exporterNet[i]->nmos[j_nmos]->m_name]["source"] = exporterNet[i]->nmos[j_nmos]->m_source;
+                j["placement"][exporterNet[i]->nmos[j_nmos]->m_name]["gate"] = exporterNet[i]->nmos[j_nmos]->m_gate;
+                j["placement"][exporterNet[i]->nmos[j_nmos]->m_name]["drain"] = exporterNet[i]->nmos[j_nmos]->m_drain;
+                j["placement"][exporterNet[i]->nmos[j_nmos]->m_name]["width"] = std::to_string(exporterNet[i]->nmos[j_nmos]->m_wide);
+            }
+        }
+
+        outFile << j.dump(4); // 4个缩进空格
+        outFile << std::endl;
+    }
+    outFile.close();
 }
+
